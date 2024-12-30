bc8466d9fc3fe6f4a111833127a372350d040896
ikkodev <rafiikkodev@gmail.com>
2024-12-30 16:45:04 +0700

    final

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: LoginPage());
+    return const MaterialApp(home: UserPage());
   }
 }
